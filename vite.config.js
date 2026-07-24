02a1511a55bde13551c2122d3d36afd7662d6f31
pankaja2328 <pankajamalshan@gmail.com>
2026-07-24 12:32:24 +0530

    Updates

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  base: '/PortFolio/', // Make sure this matches your repo name exactly (case-sensitive)
-  plugins: [react()],
-});
